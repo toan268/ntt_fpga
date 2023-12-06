58e79ed0c8cb33e89224eea4dbff399bd2452ec5
Toan Nguyen <nht.260898@gmail.com>
2023-12-06 23:47:37 +0700

    Initial code for test code

--- DE10_Standard_GHRD_WRAP/DE10_Standard_GHRD/C_CODE/main.c
+++ DE10_Standard_GHRD_WRAP/DE10_Standard_GHRD/C_CODE/main.c
@@ -20,6 +20,38 @@
 #define HW_REGS_MASK			( HW_REGS_SPAN - 1 )
 #define WRITEENABLE				( 0x00000800 )
 
+#define FI_FIFO_IN_ACLR			alt_write_word(h2f_CAM_addr + DATA_OUT_0, data)
+#define FI_FIFO_IN_WR_CLK		alt_write_word(h2f_CAM_addr + DATA_OUT_1, data)
+#define FI_FIFO_IN_RD_REQ		alt_write_word(h2f_CAM_addr + DATA_OUT_2, data)
+#define FI_FIFO_IN_WR_REQ		alt_write_word(h2f_CAM_addr + DATA_OUT_3, data)
+#define FI_FIFO_IN1_DIN			alt_write_word(h2f_CAM_addr + DATA_OUT_4, data)
+#define FI_FIFO_IN2_DIN			alt_write_word(h2f_CAM_addr + DATA_OUT_5, data)
+#define FI_FIFO_IN_MUX_RD_CLK	alt_write_word(h2f_CAM_addr + DATA_OUT_6, data)
+#define FI_FIFO_OUT_MUX_WR_CLK	alt_write_word(h2f_CAM_addr + DATA_OUT_7, data)
+#define FI_NTT_MODE				alt_write_word(h2f_CAM_addr + DATA_OUT_8, data)
+#define FI_NTT_START			alt_write_word(h2f_CAM_addr + DATA_OUT_9, data)
+#define FI_NTT_WE				alt_write_word(h2f_CAM_addr + DATA_OUT_10, data)
+#define FI_FIFO_IN_RD_CLK		alt_write_word(h2f_CAM_addr + DATA_OUT_11, data)
+#define FI_FIFO_OUT_WR_CLK		alt_write_word(h2f_CAM_addr + DATA_OUT_12, data)
+#define FI_FIFO_OUT_ACLR		alt_write_word(h2f_CAM_addr + DATA_OUT_13, data)
+#define FI_FIFO_OUT_RD_CLK		alt_write_word(h2f_CAM_addr + DATA_OUT_14, data)
+#define FI_FIFO_OUT_RD_REQ		alt_write_word(h2f_CAM_addr + DATA_OUT_15, data)
+
+#define FO_FIFO_IN1_WR_FULL		alt_read_word(h2f_CAM_addr + DATA_IN_0)
+#define FO_FIFO_IN1_WR_USED		alt_read_word(h2f_CAM_addr + DATA_IN_1)
+#define FO_FIFO_IN1_RD_EMPTY	alt_read_word(h2f_CAM_addr + DATA_IN_2)
+#define FO_FIFO_IN1_RD_USED		alt_read_word(h2f_CAM_addr + DATA_IN_3)
+#define FO_FIFO_IN2_WR_FULL		alt_read_word(h2f_CAM_addr + DATA_IN_4)
+#define FO_FIFO_IN2_WR_USED		alt_read_word(h2f_CAM_addr + DATA_IN_5)
+#define FO_FIFO_IN2_RD_EMPTY	alt_read_word(h2f_CAM_addr + DATA_IN_6)
+#define FO_FIFO_IN2_RD_USED		alt_read_word(h2f_CAM_addr + DATA_IN_7)
+#define FO_NTT_IN_DONE			alt_read_word(h2f_CAM_addr + DATA_IN_8)
+#define FO_NTT_DONE				alt_read_word(h2f_CAM_addr + DATA_IN_9)
+#define FO_FIFO_OUT3_WR_FULL	alt_read_word(h2f_CAM_addr + DATA_IN_10)
+#define FO_FIFO_OUT3_WR_USED	alt_read_word(h2f_CAM_addr + DATA_IN_11)
+#define FO_FIFO_OUT3_RD_EMPTY	alt_read_word(h2f_CAM_addr + DATA_IN_12)
+#define FO_FIFO_OUT3_RD_USED	alt_read_word(h2f_CAM_addr + DATA_IN_13)
+#define FO_FIFO_OUT3_RD_DATA	alt_read_word(h2f_CAM_addr + DATA_IN_14)
 //volatile unsigned long *h2f_CAM_search = NULL;
 volatile unsigned long *h2f_CAM_setting = NULL;
 volatile unsigned long *h2f_CAM_addr = NULL;
@@ -53,10 +85,10 @@ int main(int argc, char **argv) {
 	}
 
 	/* CODE BEGINS HERE */
-		uint32_t fifo_in;
-		uint32_t i;
+		// uint32_t fifo_in;
+		// uint32_t i;
 	
-		int enable;
+		// int enable;
 		//int32_t rule_tcam;
 		
 		/*int search;*/
@@ -71,41 +103,78 @@ int main(int argc, char **argv) {
 		
 		// Console Data input
 		printf("=============================================\n");
-		printf("========                           ==========\n");
-		printf("========   CAM's Comtroller Test   ==========\n");
-		printf("========       - Segmentation      ==========\n");
-		printf("========       - Comparation       ==========\n");
-		printf("========                           ==========\n");
+		printf("========       NTT_TEST           ==========\n");
 		printf("=============================================\n");
-		alt_write_word(h2f_CAM_addr +	DATA_OUT_1, 1);
-		alt_write_word(h2f_CAM_addr +	DATA_OUT_2, 1);
-		alt_write_word(h2f_CAM_addr +	DATA_OUT_0, 0);
-		usleep(10);
-		alt_write_word(h2f_CAM_addr +	DATA_OUT_1, 0);
-		alt_write_word(h2f_CAM_addr +	DATA_OUT_2, 0);		
-		alt_write_word(h2f_CAM_addr +	DATA_OUT_0, 1);
-		
-		printf("FIFO_DATA_OUT = %x \n",alt_read_word(h2f_CAM_addr + DATA_IN_0));
-		//usleep(100);
-		//alt_write_word(h2f_CAM_addr +	DATA_OUT_0, 0);
-		//usleep(100);
-		enable = alt_read_word(h2f_CAM_addr + DATA_IN_2);
-		while(1){
-			printf("FIFO_DATA_OUT = %x \n",alt_read_word(h2f_CAM_addr + DATA_IN_0));
-			i++;
-			if (i==1000){break;} 
+		// int function;
+		// bool running=true;
+		// uint32_t DATA_write=1;        
+		// while(running)
+		// {
+		// 	printf("Input (1-13), 0 to exit: ");
+		// 	scanf("%d", &function);
+		// 	switch(function)
+		// 	{
+		// 		case 1:
+		// 			printf("RD_CLK_0\n");
+		// 			WRITE_RD_CLK(0);
+		// 			break;
+		// 		case 2:
+		// 			printf("RD_CLK_1\n");
+		// 			WRITE_RD_CLK(1);
+		// 			break;
+		// 		case 3:
+		// 			printf("WR_CLK_0\n");
+		// 			WRITE_WR_CLK(0);
+		// 			break;
+		// 		case 4:
+		// 			printf("WR_CLK_1\n");
+		// 			WRITE_WR_CLK(1);
+		// 			break;
+		// 		case 5:
+		// 			printf("RD_REQ_0\n");
+		// 			WRITE_RD_REQ(0);
+		// 			break;
+		// 		case 6:
+		// 			printf("RD_REQ_1\n");
+		// 			WRITE_RD_REQ(1);
+		// 			break;
+		// 		case 7:
+		// 			printf("WR_REQ_0\n");
+		// 			WRITE_WR_REQ(0);
+		// 			break;
+		// 		case 8:
+		// 			printf("WR_REQ_1\n");
+		// 			WRITE_WR_REQ(1);
+		// 			break;
+		// 		case 9:
+		// 			printf("ACLR_0\n");
+		// 			WRITE_ACLR(0);
+		// 			break;
+		// 		case 10:
+		// 			printf("ACLR_1\n");
+		// 			WRITE_ACLR(1);
+		// 			break;
+		// 		case 11:
+		// 			printf("Write data %d\n",DATA_write);
+		// 			WRITE_WR_DAT(DATA_write++);
+		// 			break;
+		// 		case 12:
+		// 			printf("WR_FULL=%x\n",READ_WR_FULL);
+		// 			printf("WR_USED=%x\n",READ_WR_USED);
+		// 			break;				
+		// 		case 13:
+		// 			printf("RD_DAT=%d\n",READ_RD_DAT);
+		// 			printf("RD_EMPTY=%x\n",READ_RD_EMPTY);
+		// 			printf("RD_USED=%x\n",READ_RD_USED);
+		// 			break;
+		// 		case 0:
+		// 			printf("Exit program\n");
+		// 			running=false;
+		// 			break;
+		// 		default:
+		// 			printf("Wrong input\n");
+		// 		}
 		}
-		//alt_write_word(h2f_CAM_addr +	DATA_OUT_0, 0);
-		while(1){
-			if(enable == 1){
-				printf("FIFO_DATA_OUT = %x \n",alt_read_word(h2f_CAM_addr + DATA_IN_0));
-			}
-			else {
-				break;
-			}
-		}
-		printf("FIFO_DATA_OUT = %x \n",alt_read_word(h2f_CAM_addr + DATA_IN_0));
-		//sleep(100);
 	//* END OF CODE */
 	
 	if (munmap(virtual_base_lw, HW_REGS_SPAN) != 0) {
